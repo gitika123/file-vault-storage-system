071ea63b3511f14ded4769a3fe19e7ea15a57b23
gitika123 <gitikarath2001@gmail.com>
2026-08-31 15:45:23 -0700

    Align vault UI with BalkanID brand system

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './VaultWorkspace'
 import './styles.css'
+import './balkanid-brand.css'
 import './modal.css'
 import './filter.css'
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
