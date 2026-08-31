dd956b310442665081a4c873e474084aca274304
gitika123 <gitikarath2001@gmail.com>
2026-08-31 16:58:39 -0700

    Make theme styles consistent across routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './VaultWorkspace'
 import './styles.css'
-import './balkanid-brand.css'
 import './modal.css'
 import './filter.css'
+import './balkanid-brand.css'
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
